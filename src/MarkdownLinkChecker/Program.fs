96cf70c2e4ccf840879af2c3d580a521d56fc9ea
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-12 19:19:40 +0200

    Minor refactoring

--- src/MarkdownLinkChecker/Program.fs
+++ src/MarkdownLinkChecker/Program.fs
@@ -10,29 +10,39 @@ type Options =
       [<Value(0, Required = false, Default = "**/*.md", HelpText = "Files to check")>]
       Include: string }
 
-let private parseSuccess (options: Options) =
+type ExitCode =
+    | Success
+    | Error
+    
+let private toExitCode exitCode =
+    match exitCode with
+    | Success -> 0
+    | Error -> 1
+
+let private onSuccess (options: Options) =
     let findOptions: FindOptions =
         { Include = options.Include
           Exclude = options.Exclude }
-    
+
     let (CheckedDocuments checkedDocuments) =
         findOptions
         |> findUncheckedDocuments
         |> checkDocuments
 
-    if checkedDocuments |> List.exists (fun doc -> doc.Status = Invalid) then 1 else 0
+    let anyInvalidDocuments = checkedDocuments |> List.exists (fun doc -> doc.Status = Invalid)
+    if anyInvalidDocuments then ExitCode.Error else ExitCode.Success 
 
-let private parseFailure = 1
+let private onFailure = ExitCode.Error
 
-let private (|Success|Failure|) (result: ParserResult<Options>) =
+let private (|ParseSuccess|ParseFailure|) (result: ParserResult<Options>) =
     match result with
-    | :? (Parsed<Options>) as options -> Success options.Value
-    | _ -> Failure
-    
+    | :? (Parsed<Options>) as options -> ParseSuccess options.Value
+    | _ -> ParseFailure
+
 let private parse argv = CommandLine.Parser.Default.ParseArguments<Options>(argv)
 
 [<EntryPoint>]
 let main argv =
     match parse argv with
-    | Success options -> parseSuccess options
-    | Failure -> parseFailure
+    | ParseSuccess options -> onSuccess options |> toExitCode
+    | ParseFailure -> onFailure |> toExitCode
