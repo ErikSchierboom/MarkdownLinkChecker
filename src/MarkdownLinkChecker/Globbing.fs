8418b1dc83765ab758e36ef9dd042d0b714f8ed6
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-25 15:19:06 +0200

    More work on globbing

--- src/MarkdownLinkChecker/Globbing.fs
+++ src/MarkdownLinkChecker/Globbing.fs
@@ -1,31 +1,71 @@
 module MarkdownLinkChecker.Globbing
-//
-//open System.IO
-//open Microsoft.Extensions.FileSystemGlobbing
-//open Microsoft.Extensions.FileSystemGlobbing.Abstractions
-//
-//open MarkdownLinkChecker.Options
-//
-//type File =
-//    | MarkdownFile of string
-//    | NonMarkdownFile of string
-//
-//let private toFile (options: Options) (fileMatch: FilePatternMatch) =
-//    let path = Path.Combine(options.Directory, fileMatch.Path)
-//    let extension = Path.GetExtension(fileMatch.Path).ToLower()    
-//    if extension = ".md" then MarkdownFile path else NonMarkdownFile path
-//
-//let private createMatcher (options: Options) =
-//    Matcher()
-//        .AddInclude(options.Include)
-//        .AddExclude(options.Exclude |> Option.defaultValue "")
-//    
-//let findFiles (options: Options): File list =
-//    let matcher = createMatcher options
-//    let root = DirectoryInfoWrapper(DirectoryInfo(options.Directory))
-//    let matchResults = matcher.Execute(root)
-//
-//    matchResults.Files
-//    |> Seq.map (toFile options)
-//    |> Seq.toList
-//        
+
+open System
+open System.IO
+open System.Runtime.InteropServices
+open Microsoft.Extensions.FileSystemGlobbing
+open Microsoft.Extensions.FileSystemGlobbing.Abstractions
+
+open MarkdownLinkChecker.Options
+
+type MarkdownFile = MarkdownFile of string
+
+let private isMarkdownFile (path: string) =
+    Path.GetExtension(path) = ".md"
+
+let private toFullPath (options: Options) (path: string) =
+    Path.Combine(options.Directory, path)
+
+let private toMarkdownFile (path: string) =
+    if isMarkdownFile path then
+        Some (MarkdownFile path) 
+    else
+        None
+
+let private markdownFilesInDirectory (options: Options) =
+    let matcher = Matcher().AddInclude("**/*.md")
+    let root = DirectoryInfoWrapper(DirectoryInfo(options.Directory))
+    let matchResults = matcher.Execute(root)
+
+    matchResults.Files
+    |> Seq.map (fun fileMatch -> toFullPath options fileMatch.Path)
+    |> Seq.choose toMarkdownFile
+    
+let private markdownFilesAsIncluded (options: Options) =
+    options.Files
+    |> Seq.map (toFullPath options)
+    |> Seq.choose toMarkdownFile
+
+let private checkAllMarkdownFilesInDirectory (options: Options) =
+    List.isEmpty options.Files
+
+let private pathComparison =
+    if RuntimeInformation.IsOSPlatform(OSPlatform.Windows) then
+        StringComparison.OrdinalIgnoreCase
+    else
+        StringComparison.Ordinal
+
+let private filterExcludedMarkdownFiles (options: Options) markdownFiles =
+    let excludedFilePaths =
+        options.Exclude
+        |> Seq.map (toFullPath options)
+        |> Seq.toList
+        
+    let isExcludedFile (MarkdownFile path) =
+        excludedFilePaths
+        |> Seq.exists (fun excludePath -> path.Equals(excludePath, pathComparison))
+    
+    markdownFiles
+    |> Seq.filter (isExcludedFile >> not) 
+
+let findMarkdownFiles (options: Options): MarkdownFile list =
+    let markdownFiles = 
+        if checkAllMarkdownFilesInDirectory options then
+            markdownFilesInDirectory options
+        else
+            markdownFilesAsIncluded options
+    
+    markdownFiles
+    |> filterExcludedMarkdownFiles options
+    |> Seq.toList
+        
